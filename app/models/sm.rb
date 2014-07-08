382228315622523242c69d33858affd93ab322b5
Billy <william@busproductions.com>
2014-07-08 18:43:29 -0500

    concat bug fix

--- app/models/sm.rb
+++ app/models/sm.rb
@@ -21,7 +21,7 @@ class Sm < ActiveRecord::Base
       if next_message
         # needs to concat. append that message to this item, delete that item, and then update that sm's item and try to concat again
         if self.item
-          self.item.update_attribute(:message, "#{self.Body}#{next_message.Body}")
+          self.item.update_attribute(:message, "#{self.item.message}#{next_message.Body}")
           next_message.concatted_to_item(self.item)
         end
       end
