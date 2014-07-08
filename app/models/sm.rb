6c1c13483c837a0448ac5ef1e831f65d95c7dc1f
Billy <william@busproductions.com>
2014-07-08 18:10:56 -0500

    belongs to item

--- app/models/sm.rb
+++ app/models/sm.rb
@@ -2,6 +2,8 @@ class Sm < ActiveRecord::Base
 
   attr_accessible :AccountSid, :ApiVersion, :Body, :From, :FromCity, :FromCountry, :FromState, :FromZip, :MessageSid, :NumMedia, :SmsMessageSid, :SmsSid, :SmsStatus, :To, :ToCity, :ToCountry, :ToState, :ToZip, :item_id
 
+  belongs_to :item
+
 
   # -- ACTIONS
 
