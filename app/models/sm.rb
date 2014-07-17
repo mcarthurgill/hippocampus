1fa2f0fca3e6a0daa995a918c61c030b35037283
mcarthurgill <mickgill2008@gmail.com>
2014-07-17 11:13:24 -0500

    concatted_to_item comparing sm_id to item_id fix

--- app/models/sm.rb
+++ app/models/sm.rb
@@ -30,7 +30,11 @@ class Sm < ActiveRecord::Base
 
   def concatted_to_item i
     # delete the current item (if different from concatted to item), then update the item to the input item, then try concatting
-    if i.id != self.id
+    p "*"*50
+    p "i.id : #{i.id}"
+    p "self.id : #{self.id}"
+    p "self.item_id : #{self.item_id}"
+    if i.id != self.item_id
       self.item.delete
       self.update_attribute(:item_id, i.id)
       self.delay(run_at: 1.5.seconds.from_now).concat_if_necessary
