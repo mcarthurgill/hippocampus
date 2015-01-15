007f309312edd82bd551488a2a9e33c50f6c2e7d
mcarthurgill <mickgill2008@gmail.com>
2015-01-15 11:52:36 -0600

    fix

--- app/controllers/addons_controller.rb
+++ app/controllers/addons_controller.rb
@@ -1,8 +1,6 @@
 class AddonsController < ApplicationController
   def api_endpoint
-    Item.create(:message => params[:query][:message] + "1", :user_id => 1)
-    Item.create(:message => params[:message] + "2", :user_id => 1)
-    Item.create(:message => params.to_json, :user_id => 1)
+    Item.create(:message => params[:message], :user_id => 1)
     render :nothing => true, :status => 200
   end
 end
