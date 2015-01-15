20d791d243ede05eba32c99b314313d86fab2877
mcarthurgill <mickgill2008@gmail.com>
2015-01-15 11:48:22 -0600

    params great idea

--- app/controllers/addons_controller.rb
+++ app/controllers/addons_controller.rb
@@ -1,6 +1,8 @@
 class AddonsController < ApplicationController
   def api_endpoint
-    Item.create(:message => params[:query][:message], :user_id => 1)
+    Item.create(:message => params[:query][:message] + "1", :user_id => 1)
+    Item.create(:message => params[:message] + "2", :user_id => 1)
+    Item.create(:message => params.to_json, :user_id => 1)
     render :nothing => true, :status => 200
   end
 end
