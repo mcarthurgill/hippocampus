61a686c8e882b58707da40492cca2d389668ef0e
mcarthurgill <mickgill2008@gmail.com>
2015-01-15 13:49:32 -0600

    quick fix

--- app/controllers/addons_controller.rb
+++ app/controllers/addons_controller.rb
@@ -1,6 +1,6 @@
 class AddonsController < ApplicationController
   def api_endpoint
-    if Addon.with_addon_name_and_token(params[:addon], params[:token])
+    if Addon.for_addon_name_and_token(params[:addon], params[:token])
       Item.create_from_api_endpoint(params)  
       render :nothing => true, :status => 200
     else 
