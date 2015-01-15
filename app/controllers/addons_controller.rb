1e0be6f24d225b7d5a478ab9bfac70ce91e1fb91
mcarthurgill <mickgill2008@gmail.com>
2015-01-15 11:46:13 -0600

    params

--- app/controllers/addons_controller.rb
+++ app/controllers/addons_controller.rb
@@ -1,6 +1,6 @@
 class AddonsController < ApplicationController
   def api_endpoint
-    Item.create(:message => params[:message], :user_id => 1)
+    Item.create(:message => params[:query][:message], :user_id => 1)
     render :nothing => true, :status => 200
   end
 end
