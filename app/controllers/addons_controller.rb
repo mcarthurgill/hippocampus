5b7c1623036c799f5222bbb90c2e1cc2d8b88356
mcarthurgill <mickgill2008@gmail.com>
2015-01-15 11:36:52 -0600

    log output

--- app/controllers/addons_controller.rb
+++ app/controllers/addons_controller.rb
@@ -1,6 +1,6 @@
 class AddonsController < ApplicationController
   def api_endpoint
     Item.create(:message => params[:message], :user_id => 1)
-    render :nothing => true, :status => 200, :content_type => 'text/html'
+    render :nothing => true, :status => 200
   end
 end
