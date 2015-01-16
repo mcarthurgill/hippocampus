81480dc4a0343f93013defd9855b8565bbd45b65
mcarthurgill <mickgill2008@gmail.com>
2015-01-16 13:08:15 -0600

    addon

--- app/controllers/addons_controller.rb
+++ app/controllers/addons_controller.rb
@@ -2,7 +2,7 @@ class AddonsController < ApplicationController
   def api_endpoint
     respond_to do |format|
       if Addon.for_addon_name_and_token(params[:addon], params[:addon_token]) && i = Item.create_from_api_endpoint(params)  
-        format.html { render bucket_id: i.bucket_id, :status => 200 }
+        format.html { render bucket_id: i.bucket_id }
       else 
         format.html { render :nothing => true, :status => 401 )
       end
