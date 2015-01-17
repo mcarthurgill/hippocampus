3e959e0666121c2a4b5495b79a1b83d448cfe1b3
w <w@lxv.io>
2015-01-16 21:12:48 -0600

    itemized counting before/after saves

--- app/models/bucket_item_pair.rb
+++ app/models/bucket_item_pair.rb
@@ -8,6 +8,8 @@ class BucketItemPair < ActiveRecord::Base
   belongs_to :bucket
   belongs_to :item
 
+  after_save :handle_counts
+  after_destroy :handle_counts
 
 
   def self.with_or_create_with_params params
@@ -19,6 +21,10 @@ class BucketItemPair < ActiveRecord::Base
     bip.item.update_outstanding
     bip.bucket.increment_count
     return bip
+  end
+
+  def handle_counts
+    self.bucket.increment_count if self.bucket
   end
 
 end
