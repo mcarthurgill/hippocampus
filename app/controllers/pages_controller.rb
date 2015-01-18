b265981ffc44cc2f452c355685edf9b813a7b035
w <w@lxv.io>
2015-01-17 19:28:32 -0600

    search results

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,14 @@
 class PagesController < ApplicationController
 
+
   def search
     @active = 'search'
 
-    results = []
+    @results = []
     if params.has_key?(:t) && params[:t].strip.length > 0
       client = Swiftype::Client.new
       # get users + topics, return as one array
-      results = client.search('engine', params[:t], {:document_types => ['items','buckets'], :search_fields => { 'items' => ['message','buckets_string'], 'buckets' => ['first_name'] }, :filters => { 'items' => {'user_id' => current_user.id}, 'buckets' => {'user_id' => current_user.id} } } )
+      @results = client.search('engine', params[:t], {:document_types => ['items','buckets'], :search_fields => { 'items' => ['message','buckets_string'], 'buckets' => ['first_name'] }, :filters => { 'items' => {'user_id' => current_user.id}, 'buckets' => {'user_id' => current_user.id} } } )
     end
 
     puts results
