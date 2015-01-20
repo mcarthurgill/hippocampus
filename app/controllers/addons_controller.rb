6bb85ce701168092c06ca868d5260e1887e8fa06
mcarthurgill <mickgill2008@gmail.com>
2015-01-20 10:40:27 -0600

    warnings

--- app/controllers/addons_controller.rb
+++ app/controllers/addons_controller.rb
@@ -2,7 +2,8 @@ class AddonsController < ApplicationController
   def api_endpoint
     addon = Addon.for_addon_name_and_token(params[:addon], params[:addon_token])
     respond_to do |format|
-      if addon && resp = addon.determine_endpoint_action(params)
+      if addon
+        resp = addon.determine_endpoint_action(params)
         format.json { render json: resp, :status => 200 }
       else 
         format.json { render :nothing => true, :status => 401 }
