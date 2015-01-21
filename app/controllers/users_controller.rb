13b608e3888d2cc5d59241da29544afcd94ccba8
w <w@lxv.io>
2015-01-21 10:58:57 -0600

    event_type fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_if_not_authorized(params[:id]) ? return : nil }
-      format.json { render json: { 'Other' => @user.buckets.other_type.by_first_name,  'Person' => @user.buckets.person_type.by_first_name,  'Event' => @user.buckets.eventy_type.order('id DESC'),  'Place' => @user.buckets.place_type.by_first_name,  } }
+      format.json { render json: { 'Other' => @user.buckets.other_type.by_first_name,  'Person' => @user.buckets.person_type.by_first_name,  'Event' => @user.buckets.event_type.order('id DESC'),  'Place' => @user.buckets.place_type.by_first_name,  } }
     end
   end
 
