0b32e02bc4232820f8b89093ea7a453243fd93b4
mcarthurgill <mickgill2008@gmail.com>
2015-02-16 15:36:56 -0600

    refactored

--- app/models/twilio_messenger.rb
+++ app/models/twilio_messenger.rb
@@ -12,23 +12,27 @@ class TwilioMessenger < ActiveRecord::Base
     @account = @client.account
 
     if @body && @body.length > 160
-      message_string = ""
-      split_words = @body.split(" ")
-      split_words.each_with_index do |w, i|
-        if message_string.length + w.length + 1 < 160 #added 1 for the space
-          message_string << w + " "
-          if i == split_words.count - 1 #last word
-            @message = @account.sms.messages.create({:body => message_string, :to => append_plus_to_number(@to_number), :from => append_plus_to_number(@from_number)})  
-          end
-        else
-          @message = @account.sms.messages.create({:body => message_string, :to => append_plus_to_number(@to_number), :from => append_plus_to_number(@from_number)})
-          sleep(0.5)
-          message_string = w + " "
-        end
-      end
+      self.send_split_texts
     else
       @message = @account.sms.messages.create({:body => @body, :to => append_plus_to_number(@to_number), :from => append_plus_to_number(@from_number)})
       sleep(0.5)
+    end
+  end
+
+  def send_split_texts
+    message_string = ""
+    split_words = @body.split(" ")
+    split_words.each_with_index do |w, i|
+      if message_string.length + w.length + 1 < 160 #added 1 for the space
+        message_string << w + " "
+        if i == split_words.count - 1 #last word
+          @account.sms.messages.create({:body => message_string, :to => append_plus_to_number(@to_number), :from => append_plus_to_number(@from_number)})  
+        end
+      else
+        @account.sms.messages.create({:body => message_string, :to => append_plus_to_number(@to_number), :from => append_plus_to_number(@from_number)})
+        message_string = w + " "
+        sleep(0.5)
+      end
     end
   end
 
