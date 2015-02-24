1bea6b0325fce9d2aa402f12070cd7397757a089
mcarthurgill <mickgill2008@gmail.com>
2015-02-24 13:56:34 -0600

    item device timestamp check

--- app/models/item.rb
+++ app/models/item.rb
@@ -244,6 +244,10 @@ class Item < ActiveRecord::Base
     self.item_type == "yearly"
   end
 
+  def is_most_recent_request?(timestamp)
+    self.user.items.where("device_timestamp > ?", timestamp).empty?
+  end
+
   # -- REMINDERS
 
   def self.remind_about_events
