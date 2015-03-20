94f25b6f6b40cdf071b1b6d537774da116778618
w <w@lxv.io>
2015-03-19 19:51:23 -0500

    push notis

--- app/models/device_token.rb
+++ app/models/device_token.rb
@@ -1,5 +1,17 @@
 class DeviceToken < ActiveRecord::Base
+
   attr_accessible :android_device_token, :environment, :ios_device_token, :user_id
 
   belongs_to :user
+  has_many :push_notifications
+
+
+  def production?
+    self.environment == 'production'
+  end
+
+  def development?
+    self.environment == 'development'
+  end
+
 end
--- app/models/push_notification.rb
+++ app/models/push_notification.rb
@@ -2,15 +2,12 @@ class PushNotification < ActiveRecord::Base
 
   include ActionView::Helpers::TextHelper
 
-  attr_accessible :book_identifier, :device_token_id, :message, :push_notification_type, :status, :post_id, :environment, :topic_id
-
-
+  attr_accessible :device_token_id, :message, :badge_count, :item_id, :bucket_id, :status
 
 
   # --- ASSOCIATIONS
 
   belongs_to :device_token
-  belongs_to :post
 
 
 
@@ -19,38 +16,29 @@ class PushNotification < ActiveRecord::Base
 
   def send_notification
     #determine what type of notification, and send
-    if self.device_token.ios_device_token && self.device_token.ios_device_token.length > 0 && self.device_token.current?
-      #send ios
+    if self.device_token.ios_device_token && self.device_token.ios_device_token.length > 0
       send_ios_notification
-    end
-    if self.device_token.android_device_token && self.device_token.android_device_token.length > 0 && self.device_token.current?
-        #send android
-        send_android_notification
+    elsif self.device_token.android_device_token && self.device_token.android_device_token.length > 0
+      send_android_notification
     end
   end
 
 
   def send_ios_notification
-
     self.message = truncate(self.message, length: 160)
-
     n = Rpush::Apns::Notification.new
-    if self.device_token.environment == "production"
-      n.app = Rpush::Apns::App.find_by_name(ENV['IOS_PRODUCTION_RAPNS_ENVIRONMENT'])
+    if self.device_token.production?
+      n.app = Rpush::Apns::App.find_by_name(ENV['IOS_PRODUCTION_RAPNS'])
     else
-      n.app = Rpush::Apns::App.find_by_name(ENV['IOS_RAPNS_ENVIRONMENT'])
+      n.app = Rpush::Apns::App.find_by_name(ENV['IOS_DEVELOPMENT_RAPNS'])
     end
     n.device_token = self.device_token.ios_device_token
     n.alert = self.message
-    # if self.device_token.user
-      n.badge = 1 # self.device_token.user.likes_for_post(self.post_id)
-    # end
-    n.content_available = 1
-    if self.book_identifier
-      n.attributes_for_device = { :post_id => self.post_id, :user_id => self.device_token.user_id, :bid => self.book_identifier }
-    else
-      n.attributes_for_device = { :post_id => self.post_id, :user_id => self.device_token.user_id }
+    if self.badge_count
+      n.badge = self.badge_count
     end
+    # n.content_available = 1
+    # n.attributes_for_device = { :post_id => self.post_id, :user_id => self.device_token.user_id, :bid => self.book_identifier }
     n.save!
   end
 
