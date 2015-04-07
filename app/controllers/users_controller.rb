38ca18901e1a7a182ffaadb3547f895b712179fb
mcarthurgill <mickgill2008@gmail.com>
2015-04-07 08:52:46 -0500

    revert

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_if_not_authorized(params[:id]) ? return : nil }
-      format.json { render json: { 'Other' => @user.buckets.other_type.by_first_name.as_json(methods: :users),  'Person' => @user.buckets.person_type.by_first_name.as_json(methods: :users),  'Event' => @user.buckets.event_type.order('id DESC').as_json(methods: :users),  'Place' => @user.buckets.place_type.by_first_name.as_json(methods: :users), 'Recent' => @user.recent_buckets_with_shell } }
+      format.json { render json: { 'Other' => @user.buckets.other_type.by_first_name,  'Person' => @user.buckets.person_type.by_first_name,  'Event' => @user.buckets.event_type.order('id DESC'),  'Place' => @user.buckets.place_type.by_first_name, 'Recent' => @user.recent_buckets_with_shell } }
     end
   end
 
