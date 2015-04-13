bdd10448c6e8a834bf3737c142a7d35b5ee0bcb9
w <w@lxv.io>
2015-04-12 20:13:03 -0500

    emails

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,11 +3,12 @@ class EmailsController < ApplicationController
   # POST /emails
   # POST /emails.json
   def create
-    @email = Email.new(params)
+    # @email = Email.new(params)
+    @email = Email.new
 
     respond_to do |format|
       if @email.save
-        @email.create_item
+        # @email.create_item
         format.json { render json: @email, status: :created }
       else
         format.json { render json: @email.errors, status: :unprocessable_entity }
