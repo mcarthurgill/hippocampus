1ede63aef46ef1f3d698eeda60c0a32f393e54eb
w <w@lxv.io>
2015-04-22 16:10:54 -0500

    rescue error

--- lib/ext/string.rb
+++ lib/ext/string.rb
@@ -1,17 +1,22 @@
+
 class String
 
   def parse_for_time
-    Time.zone = 'Central Time (US & Canada)'
-    Chronic.time_class = Time.zone
-    time = Chronic.parse(self)
-    length_check = 36
-    if !time && self.length > length_check
-      time = Chronic.parse(self[(-1*length_check+2)..-1])
-    end
-    if !time && self.length > length_check
-      time = Chronic.parse(self[0...length_check])
-    end
-    return time
+    begin  
+      Time.zone = 'Central Time (US & Canada)'
+      Chronic.time_class = Time.zone
+      time = Chronic.parse(self)
+      length_check = 36
+      if !time && self.length > length_check
+        time = Chronic.parse(self[(-1*length_check+2)..-1])
+      end
+      if !time && self.length > length_check
+        time = Chronic.parse(self[0...length_check])
+      end
+      return time
+    rescue  
+      return nil
+    end 
   end
 
 end
