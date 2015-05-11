ba3cea7dc94df5571f72d639acafb2d97be18890
w <w@lxv.io>
2015-05-11 10:35:45 -0500

    by first name

--- app/models/user.rb
+++ app/models/user.rb
@@ -139,7 +139,7 @@ class User < ActiveRecord::Base
   # -- HELPERS
 
   def ungrouped_buckets
-    return self.buckets.where('"buckets"."id" NOT IN (?)', self.group_buckets.pluck(:id)).by_first_name if u.group_buckets.pluck(:id).count > 0
+    return self.buckets.where('"buckets"."id" NOT IN (?)', self.group_buckets.pluck(:id)).by_first_name if self.group_buckets.pluck(:id).count > 0
     return self.buckets.by_first_name
   end
 
