574edd88b9d511a5604f42f25a3a4cc358e8b6b0
w <w@lxv.io>
2015-05-14 16:19:42 -0500

    try to get route live

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -1,9 +1,24 @@
 class InboxController < ApplicationController
-  include Mandrill::Rails::WebHookProcessor
-  ignore_unhandled_events!
 
-  def handle_inbound(event_payload)
-    @email = Email.save_inbound_mail(event_payload)
+  # include Mandrill::Rails::WebHookProcessor
+  # ignore_unhandled_events!
+
+  # def handle_inbound(event_payload)
+    # @email = Email.save_inbound_mail(event_payload)
+  def show
+    @email = Email.new
+    respond_to do |format|
+      if @email
+        # @email.create_item
+        format.json { render json: @email, status: :created }
+      else
+        format.json { render json: @email.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def create
+    @email = Email.new
     respond_to do |format|
       if @email
         # @email.create_item
