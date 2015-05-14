168d6e43b461ce0e07bf87173fb5abfaf15b6140
w <w@lxv.io>
2015-05-14 16:21:32 -0500

    back to real processor

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -1,24 +1,10 @@
 class InboxController < ApplicationController
+  
+  include Mandrill::Rails::WebHookProcessor
+  ignore_unhandled_events!
 
-  # include Mandrill::Rails::WebHookProcessor
-  # ignore_unhandled_events!
-
-  # def handle_inbound(event_payload)
-    # @email = Email.save_inbound_mail(event_payload)
-  def show
-    @email = Email.new
-    respond_to do |format|
-      if @email
-        # @email.create_item
-        format.json { render json: @email, status: :created }
-      else
-        format.json { render json: @email.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def create
-    @email = Email.new
+  def handle_inbound(event_payload)
+    @email = Email.save_inbound_mail(event_payload)
     respond_to do |format|
       if @email
         # @email.create_item
