a703ed630c9c93b8b4dcfcda3b507ff58c09eee2
w <w@lxv.io>
2015-05-14 16:23:41 -0500

    back to real processor

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -1,7 +1,6 @@
 class InboxController < ApplicationController
-  
+
   include Mandrill::Rails::WebHookProcessor
-  ignore_unhandled_events!
 
   def handle_inbound(event_payload)
     @email = Email.save_inbound_mail(event_payload)
