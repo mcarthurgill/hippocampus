3cd9f015c134e4200b9f2a3ac8fcf82c4a100f60
w <w@lxv.io>
2015-09-10 18:21:36 -0500

    guess yearly

--- app/models/item.rb
+++ app/models/item.rb
@@ -498,7 +498,8 @@ class Item < ActiveRecord::Base
     begin  
       n = Nickel.parse self.message, DateTime.now.in_time_zone('Central Time (US & Canada)')
       if n.occurrences.count > 0 && n.occurrences.first.type.to_s != 'daily' # ----THIS WOULD BE IF YOU WANT TO EXCLUDE DATES LIKE TODAY----   && time > (Time.now+1.day).beginning_of_day
-        self.assign_attributes(reminder_date: n.occurrences.first.start_date.to_date, item_type: self.reminder_frequency_with_nickel_keyword(n.occurrences.first.type.to_s))
+        self.assign_attributes(reminder_date: n.occurrences.first.start_date.to_date)
+        self.assign_attributes(item_type: self.reminder_frequency_with_nickel_keyword(n.occurrences.first.type.to_s))
       end      
     rescue
     end 
@@ -506,6 +507,7 @@ class Item < ActiveRecord::Base
 
   def reminder_frequency_with_nickel_keyword k
     return "yearly" if self.guess_yearly_reminder? && k == 'single'
+    return "yearly" if self.reminder_date < 1.year.ago.to_date && k == 'single'
     return "once" if k == 'single'
     return "monthly" if k == 'daymonthly'
     return "monthly" if k == 'datemonthly'
