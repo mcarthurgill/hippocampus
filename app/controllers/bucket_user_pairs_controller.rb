b2ef461170b81ffa1a6e94b807e1308bf59c0967
mcarthurgill <mcarthur.gill@gmail.com>
2015-09-21 09:21:56 -0500

    send back bucket

--- app/controllers/bucket_user_pairs_controller.rb
+++ app/controllers/bucket_user_pairs_controller.rb
@@ -6,7 +6,7 @@ class BucketUserPairsController < ApplicationController
 
     respond_to do |format|
       if @bup && @bup.update_attributes(params[:bucket_user_pair])
-        format.json { render json: { :bucket_user_pair => @bup } }
+        format.json { render json: { :bucket_user_pair => @bup, :bucket => @bup.bucket.as_json(:methods => [:bucket_user_pairs, :creator]) } }
       else
         format.json { render json: @bup.errors, status: :unprocessable_entity }
       end
