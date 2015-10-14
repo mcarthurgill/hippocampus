6ad7baeb51104ac13a6eccef4b3c89b75f43bd8a
mcarthurgill <mcarthur.gill@gmail.com>
2015-10-14 10:30:53 -0400

    updated media info

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -2,7 +2,7 @@ class MediaController < ApplicationController
   # GET /media
   # GET /media.json
   def index
-    @media = Medium.all
+    @media = Medium.where("user_id != ? AND user_id != ? AND user_id != ? AND user_id != ? AND user_id != ?", 23, 2, 18, 15, 81).order('id DESC').limit(512)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -86,7 +86,7 @@ class MediaController < ApplicationController
 
     respond_to do |format|
       if @medium.update_attributes(params[:medium])
-        format.html { redirect_to @medium, notice: 'Medium was successfully updated.' }
+        format.html { redirect_to media_path, notice: 'Medium was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
