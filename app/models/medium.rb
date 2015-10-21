73f1d51f3f25d5caeb77c70cb18b4f5ce8010a83
mcarthurgill <mcarthur.gill@gmail.com>
2015-10-21 14:12:07 -0500

    m

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -111,7 +111,7 @@ class Medium < ActiveRecord::Base
         p file.tempfile.path
         p tmp_path
         p "*"*50
-        self.set_transcription_text(tmp_path)
+        # self.set_transcription_text(tmp_path)
       end
     elsif self.is_video?
       data = self.upload_video_to_cloudinary(file, public_id)
