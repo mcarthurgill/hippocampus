4a32a3848d7e714e5fdbc22951cc647f5704f91f
mcarthurgill <mcarthur.gill@gmail.com>
2015-10-22 11:57:00 -0500

    M

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -60,7 +60,7 @@ class Medium < ActiveRecord::Base
     p File.open(destiny_file_path.to_s)
     p "*"*50
     Medium.delay.set_transcription_text(medium.id, destiny_file_path.to_s)
-    Medium.delay.set_duration_test(medium.id)
+    medium.delay.set_duration_test
     puts medium.as_json().to_s
     return medium
   end
@@ -162,10 +162,9 @@ class Medium < ActiveRecord::Base
     m.save
   end
 
-  def self.set_duration_test medium_id
-    m = Medium.find(medium_id)
-    m.duration = 1.0
-    m.save
+  def set_duration_test
+    self.duration = 1.0
+    self.save
   end
 
   def upload_image_to_cloudinary(file, public_id, format)
