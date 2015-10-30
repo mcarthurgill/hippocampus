5017c47b250df64fefaae99d6cd931798f0cb3fe
w <w@lxv.io>
2015-10-30 09:46:08 -0500

    change algo

--- app/models/bucket_tag_pair.rb
+++ app/models/bucket_tag_pair.rb
@@ -4,10 +4,11 @@ class BucketTagPair < ActiveRecord::Base
   belongs_to :bucket
   belongs_to :tag
 
-  after_create :update_underlying_objects
   after_destroy :update_underlying_objects
 
   def update_underlying_objects
+    self.bucket.delay.update_tags_array if self.bucket
+    self.tag.update_number_buckets if self.tag
   end
 
 end
