53709e234a6426d00c95733fa84acbfd3bafdae1
w <w@lxv.io>
2015-11-04 15:57:37 -0600

    change local key stuff

--- app/models/link.rb
+++ app/models/link.rb
@@ -8,7 +8,7 @@ class Link < ActiveRecord::Base
 
   before_save :default_values
   def default_values
-    self.local_key ||= "link-#{self.raw_url}" if self.raw_url
+    self.local_key ||= "link-#{Time.now.to_f}-"
   end
 
   
