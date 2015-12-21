45193603ffccaf9acc6b6adbfc86377377b5ceb7
mcarthurgill <mcarthur.gill@gmail.com>
2015-12-21 15:24:14 -0600

    nudge list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -162,7 +162,7 @@ class UsersController < ApplicationController
     page = params.has_key?(:page) && params[:page].to_i > 0 ? params[:page].to_i : 0
 
     reminders = user.sorted_reminders(1000, page)
-    list = reminders.shift(1)
+    list = reminders.shift(1)[:nudges_list]
 
     respond_to do |format|
       format.json { render json: {:reminders => reminders, :nudge_list => list} }
