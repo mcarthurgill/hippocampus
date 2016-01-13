be3a9a8a3e2bc04b554f821971e44d2be03ef05b
mcarthurgill <mcarthur.gill@gmail.com>
2016-01-13 10:57:35 -0600

    m

--- public/js/server_requests.js
+++ public/js/server_requests.js
@@ -15,25 +15,41 @@ function PostToServer(url, parameters, successCallback, errorCallback) {
 }
 
 function GetToServer(url, parameters, successCallback, errorCallback) {
-  $.ajax({
-      type: 'GET',
-      url: url,
-      data: JSON.stringify(parameters),
-      contentType: 'application/javascript;',
-      dataType: 'json',
-      success: function(response){
-        successCallback(response); 
-      },
-      error: function(response){
-        errorCallback(response); 
-      }
-  });
+  if (parameters == null) {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        contentType: 'application/javascript;',
+        dataType: 'json',
+        success: function(response){
+          successCallback(response); 
+        },
+        error: function(response){
+          errorCallback(response); 
+        }
+    });  
+  } else {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        data: JSON.stringify(parameters),
+        contentType: 'application/javascript;',
+        dataType: 'json',
+        success: function(response){
+          successCallback(response); 
+        },
+        error: function(response){
+          errorCallback(response); 
+        }
+    });
+  };
+  
 }
 
 function replaceAllItems(response) {
   console.log("****SUCCESS!!!****");
   url = '/users/' + response.user_id + '/items.js'
-  GetToServer(url, { user_id: response.user_id.toString() }, doNothing, doNothing)
+  GetToServer(url, null, doNothing, doNothing)
 }
 
 function errorReplaceAllItems(response) {
