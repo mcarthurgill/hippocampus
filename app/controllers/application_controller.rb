43df729452215446316ab941ed9ff68a7c1178a1
mcarthurgill <mcarthur.gill@gmail.com>
2016-01-28 11:02:36 -0600

    m

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,15 +14,15 @@ class ApplicationController < ActionController::Base
   #   end
   # end
 
-  before_filter :set_cache_headers
+  # before_filter :set_cache_headers
 
-  private
+  # private
 
-  def set_cache_headers
-    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
-  end
+  # def set_cache_headers
+  #   response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+  #   response.headers["Pragma"] = "no-cache"
+  #   response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  # end
 
   def logged_in?
     current_user
