9c837ca744374c132a55456776be3d0cb8f35aab
mcarthurgill <mcarthur.gill@gmail.com>
2016-02-01 07:25:53 -0600

    m

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Hippocampus::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  # config.assets.precompile += %w( vendor.js vendor.css )
 
   # Generate digests for assets URLs
   config.assets.digest = true
